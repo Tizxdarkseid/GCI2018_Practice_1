4729ecdcedbd52cb1f30d816eec355016982f4dd
Tizxdarkseid <44780566+Tizxdarkseid@users.noreply.github.com>
2018-11-05 22:31:43 +0530

    Add files via upload

---
+++ gci2018.py
@@ -0,0 +1,5 @@
+for x in range(0,10):
+   	print("GCI is great")
+a=input("hey what's your name?\n")
+print("hello %s, pleased to meet you." %a)
+print("hey guess what? i reversed your name and got %s"%a[::-1])
